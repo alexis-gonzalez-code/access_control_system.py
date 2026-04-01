9160839fbdc7bdcae611e7875e893fae5377949c
alexis-gonzalez-code <otibenites939@gmail.com>
2026-03-31 23:44:03 -0600

    Update access_control_system.py

--- access_control_system.py
+++ access_control_system.py
@@ -1,5 +1,5 @@
 # le pedimos los datos al usuario 
-nombre = input("porfavoor escribe aqui tu nombre     ")
+nombre = input("porfavor escribe aqui tu nombre     ")
 edad = int(input("¿cuantos  años tienes?    "))
 
 print(f"\n  datos guardados con exito!")
@@ -7,7 +7,7 @@ print(f"\n  datos guardados con exito!")
 # paso 2: logica de decicion 
 if edad >=18:
 	                  # este bloque se ejecuta si la edad es igual o mayor a 18
-	               print(f"\nbienvenido al club " + nombre + " ya eres mayor de edad.  ")
+	 print(f"\nbienvenido al club " + nombre + " ya eres mayor de edad.  ")
 else: 	
       # este bloque se ejecuta si la condicion de arriba es falsa
- print(" lo siento, " + nombre + ". debes tener 18 años pa entrar ")                  
+ 	print(" lo siento, " + nombre + ". debes tener 18 años pa entrar ")                                   
